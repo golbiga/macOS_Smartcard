6cb2c484a46796c45a0479cbe288bd1f8625aff0
Allen Golbig <allen.m.golbig@nasa.gov>
2019-10-29 14:14:24 -0400

    idPref

---
+++ Scripts/setIdPref.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Check for logged in user will be used with security command
+loggedInUser="$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )"
+
+# Get SHA1 from export-smartcard gibberish
+sha1=$(/usr/bin/security export-smartcard -t certs | awk '/certificate #1/,/certificate #2/' | grep sha1 | head -n1| cut -d'<' -f2 | sed "s/[ >]//g")
+
+# Prompt user for Service Name, default is *.nasa.gov
+prompt () {
+	idPref=$(/usr/bin/osascript<<END
+	tell application "System Events"
+	activate
+	set the answer to text returned of (display dialog "What Identity Preference would you like to set?" default answer "*.domain.com" buttons {"Continue"})
+	end tell
+END)
+}
+
+
+# Set idPref
+setIdentity () {
+	/usr/bin/security set-identity-preference -c "$loggedInUser" -s "$idPref" -Z "$sha1"
+}
+
+prompt
+setIdentity
