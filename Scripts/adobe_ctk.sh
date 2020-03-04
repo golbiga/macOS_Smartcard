66a58e8d7d436d71a1f4d560cb513bc2e26ae1c0
Allen Golbig <allen.m.golbig@nasa.gov>
2020-03-04 13:02:33 -0500

    fixed adobe

--- Scripts/adobe_ctk.sh
+++ Scripts/adobe_ctk.sh
@@ -1,40 +1,48 @@
-#!/bin/sh
+#!/bin/zsh
 
-#Get list of non-hidden real users 
-FIND_USERS=$(dscl /Local/Default -list /Users uid | awk '$2 >= 100 && $0 !~ /^_/ { print $1 }')
+#################################################
+# Enable CTK Support for Adobe Reader & Acrobat Pro
+# Josh Harvey | Allen Golbig | March 2020
+#################################################
 
-#function to update Adobe Reader
+#Function to update Adobe Reader
 adobe_reader () {
-    su $1 -c '/usr/libexec/PlistBuddy -c "Delete :DC:Security:ASPKI:1:CTK" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK array" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: real 8" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: Dict" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK Array" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: real 0" ~/Library/Preferences/com.adobe.Reader.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: bool yes" ~/Library/Preferences/com.adobe.Reader.plist'
+    echo "Working in $userHome"
+    /usr/libexec/PlistBuddy -c "Delete :DC:Security:ASPKI:1:CTK" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK array" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: real 8" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: Dict" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK Array" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: real 0" $userHome/Library/Preferences/com.adobe.Reader.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: bool yes" $userHome/Library/Preferences/com.adobe.Reader.plist
 }
-
-#function to update Adobe Acrobat
+#Function to update Adobe Acrobat
 adobe_acrobat () {
-    su $1 -c '/usr/libexec/PlistBuddy -c "Delete :DC:Security:ASPKI:1:CTK" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK array" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: real 8" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: Dict" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK Array" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: real 0" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
-    su $1 -c '/usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: bool yes" ~/Library/Preferences/com.adobe.Acrobat.Pro.plist'
+    echo "Working in $userHome"
+    /usr/libexec/PlistBuddy -c "Delete :DC:Security:ASPKI:1:CTK" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK array" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: real 8" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK: Dict" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK Array" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: real 0" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
+    /usr/libexec/PlistBuddy -c "Add :DC:Security:ASPKI:1:CTK:1:EnableCTK: bool yes" $userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist
 }
 
-for u in $FIND_USERS; do
-    userHome=$(dscl . read /Users/$u NFSHomeDirectory | awk '{ print $NF }')
+# Updates the Plists for Acrobat and Reader for each user
+for u in $(dscl /Local/Default -list /Users uid | awk '$2 >= 100 && $0 !~ /^_/ { print $1 }'); do
+    userHome=$(dscl /Local/Default read /Users/$u NFSHomeDirectory | awk '{ print $NF }')
     if [[ -e "$userHome/Library/Preferences/com.adobe.Acrobat.Pro.plist" ]]; then
-        echo "updating Acrobat settings for $u"
+        echo "Updating Acrobat settings for $u"
         adobe_acrobat $u 
+    else
+        echo "Acrobat not found for $u."
     fi
-    
+     
     if [[ -e "$userHome/Library/Preferences/com.adobe.Reader.plist" ]]; then
-        echo "updating Reader settings for $u"
+        echo "Updating Reader settings for $u"
         adobe_reader $u
+    else
+      echo "Reader not found for $u"
     fi   
 done
 
