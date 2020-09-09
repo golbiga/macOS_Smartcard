c32af408cbc699f8235d760ea417f765ccb8b4bb
Allen Golbig <allen.m.golbig@nasa.gov>
2020-09-09 11:31:36 -0400

    Better awk

--- Scripts/setIdPref.sh
+++ Scripts/setIdPref.sh
@@ -4,7 +4,7 @@
 loggedInUser="$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )"
 
 # Get SHA1 from export-smartcard gibberish
-sha1=$(/usr/bin/security export-smartcard -t certs | awk '/certificate #1/,/certificate #2/' | grep sha1 | head -n1| cut -d'<' -f2 | sed "s/[ >]//g")
+sha1=$(/usr/bin/security export-smartcard -t certs | awk '/Certificate For PIV Authentication/,/sha1/' | grep sha1 | head -n1| cut -d'<' -f2 | sed "s/[ >]//g")
 
 # Prompt user for Service Name, default is *.domain.com
 prompt () {
