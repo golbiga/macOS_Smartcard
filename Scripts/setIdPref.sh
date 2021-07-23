e7be618e8ead9285c33e19d7096ff2616972c7ef
Allen Golbig <allen.m.golbig@nasa.gov>
2021-07-23 09:33:00 -0400

    updated idPref

--- Scripts/setIdPref.sh
+++ Scripts/setIdPref.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
 # Check for logged in user will be used with security command
-loggedInUser="$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )"
+loggedInUser="$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }'  )"
 
 # Get SHA1 from export-smartcard gibberish
 sha1=$(/usr/bin/security export-smartcard -t certs | awk '/Certificate For PIV Authentication/,/sha1/' | grep sha1 | head -n1| cut -d'<' -f2 | sed "s/[ >]//g")
 
-# Prompt user for Service Name, default is *.domain.com
-prompt () {
-	idPref=$(/usr/bin/osascript<<END
-	tell application "System Events"
-	activate
-	set the answer to text returned of (display dialog "What Identity Preference would you like to set?" default answer "*.domain.com" buttons {"Continue"})
-	end tell
-END)
-}
 
+# Prompt user for Service Name, default is *.nasa.gov
+prompt () {
+	idPref="$(/usr/bin/osascript -e 'display dialog "What Identity Preference would you like to set?" default answer "*.domain.com" buttons {"Continue"}' | /usr/bin/awk -F':' '{print $3}')"
+}
 
 # Set idPref
 setIdentity () {
