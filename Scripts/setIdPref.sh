b6fb7dce8ac9f3c363e196a24bde9f5f68c62caf
Allen Golbig <golbiga@gmail.com>
2021-09-29 19:37:57 -0400

    Update setIdPref.sh

--- Scripts/setIdPref.sh
+++ Scripts/setIdPref.sh
@@ -7,7 +7,7 @@ loggedInUser="$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && !
 sha1=$(/usr/bin/security export-smartcard -t certs | awk '/Certificate For PIV Authentication/,/sha1/' | grep sha1 | head -n1| cut -d'<' -f2 | sed "s/[ >]//g")
 
 
-# Prompt user for Service Name, default is *.nasa.gov
+# Prompt user for Service Name, default is *.domain.com
 prompt () {
 	idPref="$(/usr/bin/osascript -e 'display dialog "What Identity Preference would you like to set?" default answer "*.domain.com" buttons {"Continue"}' | /usr/bin/awk -F':' '{print $3}')"
 }
