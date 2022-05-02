f6ef51914d9f2c16bd0ef1cd4659c5eb133c50cf
Allen Golbig <golbiga@gmail.com>
2022-05-02 12:40:25 -0400

    fixed awk

--- Scripts/smartcard_recovery.sh
+++ Scripts/smartcard_recovery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Smartcard Recovery for macOS 10.15.7+
-#rev:2.2
+#rev:2.2.1
 
 # Prompt user for account name:
 userPrompt="Please enter the account that requires an exemption from smartcard enforcement: "
@@ -10,21 +10,21 @@ printf "\e[1m$userPrompt"
 read uid
 
 # Check for Boot Volume Name. User may have changed from Macintosh HD:
-bootVolumeName=$(/usr/sbin/diskutil apfs listGroups | /usr/bin/awk -F: '/(System)/ { getline; gsub(/^[ \t]+/,"",$2); print $2}')
+bootVolumeName=$(/usr/sbin/diskutil apfs listGroups | /usr/bin/awk -F: '/(System)/ { getline; gsub(/^[ \t]+/,"",$2); print "/Volumes/"$2}')
 if [[ -z "$bootVolumeName" ]]; then
     echo "Boot Volume not found. Please verify using the Startup Disk menu bar item and try again."
     exit 1
 fi
 
 # Check if user is already exempt:
-userExempt=$(/usr/bin/defaults read "/Volumes/$bootVolumeName/var/db/dslocal/nodes/Default/users/$uid" SmartCardEnforcement 2>/dev/null | /usr/bin/awk 'NR==2' | /usr/bin/sed 's/^[ \t]*//')
+userExempt=$(/usr/bin/defaults read "$bootVolumeName"/var/db/dslocal/nodes/Default/users/"$uid" SmartCardEnforcement 2>/dev/null | /usr/bin/awk 'NR==2' | /usr/bin/sed 's/^[ \t]*//')
 if [[ "$userExempt" == "2" ]]; then
   echo "$uid is already exempt."
   exit 0
 fi
 
 # Disable SmartCardEnforcement by setting it in User account:
-/usr/bin/defaults write "/Volumes/$bootVolumeName/var/db/dslocal/nodes/Default/users/$uid" SmartCardEnforcement -array-add 2
+/usr/bin/defaults write "$bootVolumeName/var/db/dslocal/nodes/Default/users/$uid" SmartCardEnforcement -array-add 2
 
 # Disable SmartCardEnforcement by setting it in User account:
 arch=$(/usr/bin/arch)
@@ -36,7 +36,7 @@ fi
 # Exemption is removed after 1 hour
 createRemovalLaunchDaemon (){
 local launch_daemon="com.company.smartcard.exemption"
-local launch_daemon_path="/Volumes/$bootVolumeName/Library/LaunchDaemons/$launch_daemon".plist
+local launch_daemon_path="$bootVolumeName"/Library/LaunchDaemons/"$launch_daemon".plist
 
 echo "<?xml version="1.0" encoding="UTF-8"?>
 <plist version="1.0">
